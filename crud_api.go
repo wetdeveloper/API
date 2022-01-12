34b1f57ae3fc53531363d3c9f31b906ea12c64db
Ehsan Heydary <58146358+wetdeveloper@users.noreply.github.com>
2022-01-12 14:33:30 +0330

    Update crud_api.go

--- crud_api.go
+++ crud_api.go
@@ -11,6 +11,32 @@ func Num(c echo.Context) error {
 	return c.String(http.StatusOK, num)
 }
 
+
 func CrudForm(c echo.Context) error {
 	return c.Render(http.StatusOK, "crudPage.html", map[string]interface{}{})
 	}
+
+//Create,update,Delete
+func Cud(c echo.Context) error {
+			operation := c.FormValue("operation")
+			username := c.FormValue("username")
+			password := c.FormValue("password")
+			if operation == "C" {
+				if connection.InsertUser(mydb, username, password) {
+					return c.String(http.StatusOK, "Created")
+				}
+				return c.String(http.StatusOK, "There is an error")
+			} else if operation == "U" {
+				if connection.UpdateUser(mydb, username, password) {
+					return c.String(http.StatusOK, "Updated")
+				}
+				return c.String(http.StatusOK, "There is an error")
+			} else if operation == "D" {
+				if connection.DeleteUser(mydb, username) {
+					return c.String(http.StatusOK, "Deleted")
+				}
+				return c.String(http.StatusOK, "There is an error")
+			}
+			return c.String(http.StatusOK, "Not valid operation")
+
+		}
