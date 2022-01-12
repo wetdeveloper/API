1cfb41ec461479b7d953abeab91ea5e2cd44adb1
Ehsan Heydary <58146358+wetdeveloper@users.noreply.github.com>
2022-01-12 21:28:50 +0330

    Update crud_api.go

--- crud_api.go
+++ crud_api.go
@@ -8,10 +8,6 @@ import (
 	"github.com/wetdeveloper/connection"
 )
 
-func Num(c echo.Context) error {
-	num := c.Param("num")
-	return c.String(http.StatusOK, num)
-}
 
 func CrudForm(c echo.Context) error {
 	return c.Render(http.StatusOK, "crudPage.html", map[string]interface{}{})
